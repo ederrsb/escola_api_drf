c9330f58fd7a2f1b290a97a41537619e89e9bae0
ederrsb <ederrsb@gmail.com>
2024-05-01 12:34:28 -0300

    Viewset, Urls

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import path, include
+from escola.views import AlunosViewSet, CursosViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, 'Alunos')
+router.register('cursos', CursosViewSet, 'Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos),
+    path('', include(router.urls)),
 ]
