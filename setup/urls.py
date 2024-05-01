3d58fab550e091bd3aa689101b2fbc28e78e3992
ederrsb <ederrsb@gmail.com>
2024-05-01 13:39:51 -0300

    Matricula - ForeignKey

--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunosViewSet, CursosViewSet
+from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, 'Alunos')
 router.register('cursos', CursosViewSet, 'Cursos')
+router.register('matriculas', MatriculasViewSet, 'Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
